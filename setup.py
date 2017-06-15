d45d1966c4a69a97946a1502ca32d6a5fc557fed
Laurent Lasudry <lasudry@gmail.com>
2017-06-15 10:03:38 +0200

    Back to development: 0.4.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Installer for the cpskin.diazotheme.dreamRightPortlet package."""
 
-version = '0.4.1'
+version = '0.4.2.dev0'
 
 from setuptools import find_packages
 from setuptools import setup
